acbc64dbdf8f1cbc1b7cfdc456e3cede2348e473
JiaXing <s3a632055@gm.student.ncut.edu.tw>
2019-10-19 17:37:53 +0800

    <修改>Iindex

--- public/index.php
+++ public/index.php
@@ -5,12 +5,12 @@ use Demo\Hello\Lara;
 use Demo\Hello;
 
 
-$world = new World();
-$lara= new Lara();
-$vincent= new Hello\Someone('Vincent');
+//$world = new World();
+//$lara= new Lara();
+//$vincent= new Hello\Someone('Vincent');
 // 以下Someone類別的使用可以不用use Demo\Hello;
-$mary= new \Demo\Hello\Someone('Mary');
-$john= new Demo\Hello\Someone('John');
+//$mary= new \Demo\Hello\Someone('Mary');
+//$john= new Demo\Hello\Someone('John');
 
 
 use Monolog\Logger;
